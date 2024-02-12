0a2055e437c3555265b22a8f0a6efe02cf2597af
QuangNam203 <quangnam2ko3@gmail.com>
2024-02-13 00:23:21 +0700

    Xong SignIn - SignUp

--- src/Components/Pages/Login/SignInUpForm.jsx
+++ src/Components/Pages/Login/SignInUpForm.jsx
@@ -4,7 +4,32 @@ import * as Yup from 'yup';
 import Alert from '@mui/material/Alert';
 import './styles.css'
 
+
 function SignInUpForm(){
+
+    const handleChangeSignIp = ()=>{
+        const container = document.querySelector(".container");
+        const sign_in_btn = document.querySelector("#sign-in-btn");
+        container.classList.remove("sign-up-mode")
+    }
+
+    const handleChangeSignUp = ()=>{
+        const container = document.querySelector(".container");
+        const sign_up_btn = document.querySelector("#sign-up-btn");
+        container.classList.add("sign-up-mode")
+    }
+
+    const handlchangeSignUpBtn2 = ()=>{
+        const container = document.querySelector(".container");
+        const sign_up_btn2 = document.querySelector("#sign-up-btn2");
+        container.classList.add("sign-up-mode2");
+    }
+
+    const handlchangeSignInBtn2 = ()=>{
+        const container = document.querySelector(".container");
+        const sign_in_btn2 = document.querySelector("#sign-in-btn2");
+        container.classList.remove("sign-up-mode2");
+    }
 
     return(
         <Formik
@@ -74,14 +99,14 @@ function SignInUpForm(){
                         <ion-icon name="logo-instagram"></ion-icon>
                         </a>
                     </div>
-                    <p class="account-text">Don't have an account? <a href="#" id="sign-up-btn2">Sign Up</a></p>
+                    <p class="account-text">Don't have an account? <a href="#" id="sign-up-btn2" onClick={handlchangeSignUpBtn2}>Sign Up</a></p>
                 </Form>
                 <Form className='sign-up-form'>
                     <h2 class="title">Sign in</h2>
                     <FastField name="username">
                     {({ field, form, meta }) => (
                         <div class="input-field">
-                        <i class="fas fa-user"></i>
+                        <ion-icon name="person"></ion-icon>
                         <input type="text" placeholder="Username" {...field}/>
                         {meta.touched && meta.error && <div className='error'>{meta.error}</div>}
                         </div>
@@ -90,7 +115,7 @@ function SignInUpForm(){
                     <FastField name="email">
                     {({ field, form, meta }) => (
                         <div class="input-field">
-                        <i class="fas fa-user"></i>
+                        <ion-icon name="mail"></ion-icon>
                         <input type="text" placeholder="Email" {...field}/>
                         {meta.touched && meta.error && <div className='error'>{meta.error}</div>}
                         </div>
@@ -99,7 +124,7 @@ function SignInUpForm(){
                     <FastField name="password">
                     {({ field, form, meta }) => (
                         <div class="input-field">
-                        <i class="fas fa-user"></i>
+                        <ion-icon name="lock-closed"></ion-icon>
                         <input type="text" placeholder="Password" {...field}/>
                         {meta.touched && meta.error && <div>{meta.error}</div>}
                         </div>
@@ -109,19 +134,19 @@ function SignInUpForm(){
                     <p class="social-text">Or Sign in with social platform</p>
                     <div class="social-media">
                         <a href="#" class="social-icon">
-                        <ion-icon name="logo-facebook"></ion-icon>
+                            <ion-icon name="logo-facebook"></ion-icon>
                         </a>
                         <a href="" class="social-icon">
-                        <ion-icon name="logo-twitter"></ion-icon>
+                            <ion-icon name="logo-twitter"></ion-icon>
                         </a>
                         <a href="" class="social-icon">
-                        <ion-icon name="logo-google"></ion-icon>
+                            <ion-icon name="logo-google"></ion-icon>
                         </a>
                         <a href="" class="social-icon">
-                        <ion-icon name="logo-instagram"></ion-icon>
+                            <ion-icon name="logo-instagram"></ion-icon>
                         </a>
                     </div>
-                    <p class="account-text">Already have an account? <a href="#" id="sign-up-btn2">Sign In</a></p>
+                    <p class="account-text">Already have an account? <a href="#" id="sign-in-btn2" onClick={handlchangeSignInBtn2}>Sign In</a></p>
                 </Form>
             </div>
             <div class="panels-container">
@@ -129,7 +154,7 @@ function SignInUpForm(){
                     <div class="content">
                         <h3>Member of Brand?</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque accusantium dolor, eos incidunt minima iure?</p>
-                        <button class="btn" id="sign-in-btn">Sign in</button>
+                        <button class="btn" id="sign-in-btn" onClick={handleChangeSignIp}>Sign in</button>
                     </div>
                     <img src="signin.svg" alt="" class="image"/>
                 </div>
@@ -137,7 +162,7 @@ function SignInUpForm(){
                         <div class="content">
                             <h3>New to Brand?</h3>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque accusantium dolor, eos incidunt minima iure?</p>
-                            <button class="btn" id="sign-up-btn">Sign up</button>
+                            <button class="btn" id="sign-up-btn" onClick={handleChangeSignUp}>Sign up</button>
                         </div>
                         <img src="signup.svg" alt="" class="image"/>
                     </div>
